b1edc58bf0b59e7a4c777986f1ab1d41175a40f3
abigailvolk <114170078+abigailvolk@users.noreply.github.com>
2024-01-18 20:27:18 -0700

    Added function that reads data from WQP

--- R/input-utils.R
+++ R/input-utils.R
@@ -1,6 +1,6 @@
 #' Saves the GYRN historical and current WQ sites as a list.
 #'
-#' @param sites Optional. A vector of WQP of site IDs,
+#' @param sites A vector of WQP of site IDs,
 #' e.g. c("11NPSWRD_WQX-GRTE_SNR01", "11NPSWRD_WQX-GRTE_SNR02"). Defaults to GRYN WQ sites.
 #'
 #' @return A list of the site names, where each element of the list is a site name.
@@ -22,3 +22,20 @@ generate_gryn_sites <- function(sites = c("11NPSWRD_WQX-GRTE_SNR01",
   as.list(sites)
 }
 
+
+#' Pull Site Data from the WQ Portal
+#'
+#' @param site_list A list of WQP site IDs
+#' @param data_profile The type of data to pull. Default is physical/chemical.
+#'
+#' @return A list of WQP data frames for input site IDs
+#' @export
+#'
+#' @examples
+#' site_name_list <- as.list(c("11NPSWRD_WQX-GRTE_SNR01", "11NPSWRD_WQX-GRTE_SNR02"))
+#' site_df_list <- grab_wq_data(site_name_list)
+grab_wq_data <- function(site_list, data_profile = "resultPhysChem"){
+  data <- site_list |>
+    purrr::map(\(x) dataRetrieval::readWQPdata(siteid = x,
+                                               dataProfile = data_profile))
+}
